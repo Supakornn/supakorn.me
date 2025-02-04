018386c4cbdbbf165593081b1eafaf856ac02d59
Supakorn <supakorn0728@gmail.com>
2025-02-04 07:49:00 +0700

    :rocket: add responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -13,6 +13,13 @@ export default {
         },
       },
     },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 };
