1b960ffb318f8ee1bca9eb0abc8979012f6ceb4f
Supakorn <supakorn0728@gmail.com>
2025-02-04 07:00:47 +0700

    :rocket: new version

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
-    fontFamily: {
-      display: ["Comic Neue", "cursive"]
-    }
+    extend: {
+      colors: {
+        primary: "#9FEF00",
+        dark: {
+          DEFAULT: "#141D2B",
+          lighter: "#1A2332",
+          card: "#1E2936",
+        },
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
