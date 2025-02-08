68ec852f51f9c8b82496c51ad1e4b756bab8afc8
Supakornn <supakorn0728@gmail.com>
2025-02-09 06:44:45 +0700

    :sparkles: refactor & add loadingpage

---
+++ src/components/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as GithubIcon } from "./GithubIcon.astro";
+export { default as LinkedInIcon } from "./LinkedInIcon.astro";
+export { default as InstagramIcon } from "./InstagramIcon.astro";
+export { default as LinkIcon } from "./LinkIcon.astro";
+export { default as DownloadIcon } from "./DownloadIcon.astro";
